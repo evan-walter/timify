87b3c0c81fb9c9a85018623320846231d1101d8e
Evan Walter <evan.walter.software@gmail.com>
2022-11-05 19:07:12 -0700

    working with google map packages

--- components/App.js
+++ components/App.js
@@ -1,26 +1,29 @@
 import { useState, useEffect, useRef } from 'react'
 import {
   GoogleMapProvider,
-  useGoogleMap,
+  usePlacesService,
 } from '@ubilabs/google-maps-react-hooks'
 
 const mapOptions = {
   zoom: 12,
   center: {
-    lat: 43.68,
-    lng: -79.43,
+    lat: 40.59,
+    lng: -122.39,
   },
 }
 
 export default function App() {
   const [mapContainer, setMapContainer] = useState(null)
+  const placesService = usePlacesService()
+
   return (
     <GoogleMapProvider
       googleMapsAPIKey={process.env.NEXT_PUBLIC_API_KEY}
       options={mapOptions}
       mapContainer={mapContainer}
     >
-      <div ref={(node) => setMapContainer(node)} style={{ height: '100vh' }} />
+      {/* <div ref={(node) => setMapContainer(node)} style={{ height: '100vh' }} /> */}
+      <button onClick={() => console.log(placesService)}>Log</button>
     </GoogleMapProvider>
   )
 }
