93fa53922d7092878fc3ab08ac9f6db5a0504503
Evan Walter <evan.walter.software@gmail.com>
2022-11-05 17:58:21 -0700

    convert from ts to just js

--- components/App.js
+++ components/App.js
@@ -1,3 +1,14 @@
+import { useMemo } from 'react'
+// import { GoogleMap } from '@react-google-maps/api'
+// import { useLoadScript } from '@react-google-maps/api'
+// import { Marker } from '@react-google-maps/api'
+
 export default function App() {
+  // const { isLoaded } = useLoadScript({
+  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  // })
+
+  // if (!isLoaded) return <div>Loading...</div>
+
   return <div>App</div>
 }
