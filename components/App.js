ec9f00a43ae36e2acc4d4329546cc978013a83ff
Evan Walter <evan.walter.software@gmail.com>
2022-11-05 18:42:37 -0700

    render map

--- components/App.js
+++ components/App.js
@@ -1,14 +1,26 @@
-import { useMemo } from 'react'
-// import { GoogleMap } from '@react-google-maps/api'
-// import { useLoadScript } from '@react-google-maps/api'
-// import { Marker } from '@react-google-maps/api'
+import { useState, useEffect, useRef } from 'react'
+import {
+  GoogleMapProvider,
+  useGoogleMap,
+} from '@ubilabs/google-maps-react-hooks'
+
+const mapOptions = {
+  zoom: 12,
+  center: {
+    lat: 43.68,
+    lng: -79.43,
+  },
+}
 
 export default function App() {
-  // const { isLoaded } = useLoadScript({
-  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  // })
-
-  // if (!isLoaded) return <div>Loading...</div>
-
-  return <div>App</div>
+  const [mapContainer, setMapContainer] = useState(null)
+  return (
+    <GoogleMapProvider
+      googleMapsAPIKey={process.env.NEXT_PUBLIC_API_KEY}
+      options={mapOptions}
+      mapContainer={mapContainer}
+    >
+      <div ref={(node) => setMapContainer(node)} style={{ height: '100vh' }} />
+    </GoogleMapProvider>
+  )
 }
